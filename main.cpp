ffd99c12b8c35e58bd95f8e9529bb721b0be2976
Adriel Guerrero <aguerrero4109@sdsu.edu>
2025-12-05 19:05:48 -0800

    out of bounds and walls testing

--- main.cpp
+++ main.cpp
@@ -117,11 +117,23 @@ void printPath(pair<int,int> exitcell,
 // STUDENTS IMPLEMENT DFS HERE
 // Add arguments, return type, and logic
 // ----------------------------------------------------------
-// bool dfs(……) {
-//     // Your code here
-// }
+   bool dfs(int r, int c, const vector<vector<int>>& maze, vector<vector<bool>>& visited,
+       vector<vector<int>>& parent_r, vector<vector<int>>& parent_c,
+       int exit_r, int exit_c) {
+       // Your code here
+    int mazeRows = maze.size();
+    int mazeCols = maze[0].size();
 
-
+    cout << "testing " << r << ", " << c <<"\n";
+    if (r<0 || c<0 || r>=mazeRows || c>=mazeCols) {//out of bounds check
+        cout << " out of bound";
+        return false;
+    }
+    if (maze[r][c] == 1) {//wall check
+        cout << "wall";
+        return false;
+    }
+   }
 // ----------------------------------------------------------
 // MAIN PROGRAM (students add DFS calls and logic)
 // ----------------------------------------------------------
@@ -159,17 +171,18 @@ int main() {
     // STUDENT WORK:
     // Call your DFS, track visited, and fill parent_r and parent_c
     // ------------------------------------------------------
-    // bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
+    // bool found = dfs(-1, 0, maze, visited, parent_r, parent_c, exit_r, exit_c);
+       bool found = dfs(0, 0, maze, visited, parent_r, parent_c, exit_r, exit_c);
 
     // ------------------------------------------------------
     // STUDENT WORK:
     // If found, print the path
     // ------------------------------------------------------
-    // if (found) {
+       if (found) {
     //     printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
-    // } else {
-    //     cout << "\nNo path exists.\n";
-    // }
+       } else {
+           cout << "\nNo path exists.\n";
+       }
 
     return 0;
 }
